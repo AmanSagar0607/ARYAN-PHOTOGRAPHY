dd741ba9049344f7533c8937d448ca304d9a2939
luckysagar6265 <62849475+luckysagar6265@users.noreply.github.com>
2021-11-28 23:15:55 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,35 @@
+
+
+// testemonials
+
+jQuery(document).ready(function($) {
+  "use strict";
+  //  TESTIMONIALS CAROUSEL HOOK
+  $('#customers-testimonials').owlCarousel({
+      loop: true,
+      center: true,
+      items: 3,
+      margin: 0,
+      autoplay: true,
+      dots:true,
+      autoplayTimeout: 8500,
+      smartSpeed: 450,
+      responsive: {
+        0: {
+          items: 1
+        },
+        768: {
+          items: 2
+        },
+        1170: {
+          items: 3
+        }
+      }
+  });
+});
+
+
+
+
+// preloader
+
